e8f267f02f9151f5eca2caf787fbd9ad4da83629
Aya Nasser <ayanasserabdelfattah@gmail.com>
2026-08-21 20:22:43 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,11 +10,24 @@ $(document).click(function (e) {
 const switchInput = document.getElementById('input');
 
 
+// switchInput.addEventListener('change', () => {
+
+
+// document.body.classList.toggle('dark' , switchInput.checked);
+// });
+
+// لما الصفحة تفتح
+if (localStorage.getItem('darkMode') === 'true') {
+    document.body.classList.add('dark');
+    switchInput.checked = true;
+}
+
 switchInput.addEventListener('change', () => {
+    document.body.classList.toggle('dark', switchInput.checked);
 
-
-document.body.classList.toggle('dark' , switchInput.checked);
+    localStorage.setItem('darkMode', switchInput.checked);
 });
+
 
 
 
